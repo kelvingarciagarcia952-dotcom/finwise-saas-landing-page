fc179e3a4e018c9882c435d71433b96cc865556b
Kellgreat7 <kelvingarciagarcia952@gmail.com>
2025-09-05 12:16:27 -0400

    Create gigCategories.ts

---
+++ src/data/gigCategories.ts
@@ -0,0 +1,9 @@
+// src/data/gigCategories.ts
+export const gigCategories = [
+  'Diseño Gráfico',
+  'Desarrollo Web',
+  'Marketing Digital',
+  'Redacción y Traducción',
+  'Consultoría',
+  'Otro',
+];
