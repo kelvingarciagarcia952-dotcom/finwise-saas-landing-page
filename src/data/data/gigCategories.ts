b84bbb9861c25a58858215547dd33afcc923fed4
Kellgreat7 <kelvingarciagarcia952@gmail.com>
2025-09-05 12:14:04 -0400

    Delete src/data/data directory

--- src/data/data/gigCategories.ts
+++
@@ -1,9 +0,0 @@
-// src/data/gigCategories.ts
-export const gigCategories = [
-  'Diseño Gráfico',
-  'Desarrollo Web',
-  'Marketing Digital',
-  'Redacción y Traducción',
-  'Consultoría',
-  'Otro',
-];
