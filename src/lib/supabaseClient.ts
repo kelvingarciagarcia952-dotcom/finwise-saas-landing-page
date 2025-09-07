53a4c775b4864e5b0977189388c59106ecc23cc8
Kellgreat7 <kelvingarciagarcia952@gmail.com>
2025-09-06 21:42:33 -0400

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 
+// Aquí usamos createClient para crear la instancia supabase
 export const supabase = createClient(supabaseUrl, supabaseKey);
